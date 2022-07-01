51b7e87bfaea9cd1877e7d2df4d5ab4dc24a7e78
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-07-02 04:40:55 +0700

    creating bubble for tags, disable bold

--- components/postCard.tsx
+++ components/postCard.tsx
@@ -19,21 +19,28 @@ const PostCard = ({ post }) => {
 
 const PostCardPage = ({ post }) => {
   // console.log("==================");
-  // console.log("post", post);
+  post.properties.tags.multi_select.map((e) => {
+    console.log("e", e);
+  });
   const date = new Date(post.last_edited_time).toLocaleString("en-US", {
     month: "short",
     day: "2-digit",
   });
   return (
     <Link key={post.id} href={`/${post.id}`}>
-      <div className="hover:bg-neutral-300 p-2 hover:scale-[1.01] hover:duration-75 text-base mx-1 mb-1">
+      <div className="hover:bg-neutral-300 p-2 hover:scale-[1.01] hover:duration-75 text-base mx-1 mb-1 rounded-md">
         {post.cover ? (
           <div className="flex">
             <div className="basis-3/4">
-              <div className="font-bold">
+              <div className="">
                 <Text text={post.properties.Name.title} />
               </div>
-              <div className="text-neutral-500 text-xs">{date}</div>
+              <div className="flex">
+                <div className="text-neutral-500 text-xs">{date}</div>
+                {post.properties.tags.multi_select.map((e) => {
+                  <div>{e.name}</div>;
+                })}
+              </div>
             </div>
             <div className="basis-1/4">
               <img src={post.cover.file.url} alt="Cover Image" className="h-9 w-10 object-contain" />
@@ -41,10 +48,15 @@ const PostCardPage = ({ post }) => {
           </div>
         ) : (
           <div>
-            <div className="font-bold">
+            <div className="">
               <Text text={post.properties.Name.title} />
             </div>
-            <div className="text-neutral-500 text-xs">{date}</div>
+            <div className="flex">
+              <div className="text-neutral-500 text-xs basis-1/6">{date}</div>
+              {post.properties.tags.multi_select.map((e: any) => {
+                return <div className="text-3xs text-neutral-500 border-2 rounded-full px-1 mx-1 border-neutral-300">{e.name}</div>;
+              })}
+            </div>
           </div>
         )}
       </div>
@@ -61,7 +73,7 @@ const PostCardPageless = ({ post }) => {
   });
   return (
     <Link key={post.id} href={`/${post.id}`}>
-      <div className="p-2 border-l-4 border-l-neutral-300 text-lg mb-7">
+      <div className="p-2 border-l-4 border-l-neutral-300 text-base mb-7">
         {post.cover && (
           <div className="w-full">
             <img src={post.cover.file.url} alt="Cover Image" />
